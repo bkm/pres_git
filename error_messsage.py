ed75878c5608f42ea5787a51f9cf0c5ac4c4ad33
bastian <bastian.marquis@chuv.ch>
2021-10-07 12:50:37 +0200

    First commit

---
+++ error_messsage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+class ThisIsNotAClass:
+    def __init__(self):
+        print("Hello world")
+
+    def my_fancy_method(self):
+        raise Exception("Error: you should pull your lower lip over your head and swallow!")
